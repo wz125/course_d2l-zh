d5aa740d054a227c6c5b8fe075db502da285861a
wz125 <wzs125@gmail.com>
2019-01-22 14:27:30 +0000

    by wangzh  Tue Jan 22 14:27:30 UTC 2019

--- commit.sh
+++ commit.sh
@@ -1,4 +1,20 @@
 
+#conda config --prepend channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+#conda config --prepend channels http://mirrors.ustc.edu.cn/anaconda/pkgs/free/
+#conda env create -f environment.yml
+#source activate gluon
+#
+
+#env: jupyter
+#jupyter notebook --generate-config
+#$ ipython
+#from notebook.auth import passwd
+#passwd()
+#Enter password:
+#Verify password:
+#Out[2]: 'sha1:6f6193fcfbd5:614c4ba185334868fc8bbce2e9890b3ef7d1a79b'  
+#jupyter notebook
+
 git add -u ./*
 git commit -m "by wangzh  `date`"
 git push origin master
